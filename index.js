0f430b2dd2c0959ee51a018bfdec841057586845
Tejas Kevate <111335831+Tejaskevate89@users.noreply.github.com>
2025-03-25 10:20:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+gsap.from(".nav", {
+    y:-30,
+    duration:1,
+    stagger:1,
+    opacity:0
+})
+
+gsap.from(".hero h1", {
+    duration:3,
+    stagger:1,
+    opacity:0,
+    delay:1,
+    scale:0
+})
+
+gsap.from(".hero h1", {
+    scale:3,
+})
+
+gsap.from(".hero p", {
+    x:50,
+    duration:3,
+    stagger:1,
+    opacity:0,
+    delay:2,
+    scale:0
+})
+
+
